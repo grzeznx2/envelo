b4c933b8b293f2fb3a8610d2099ebd77723a3f3a
grzeznx2 <grzeznx2@gmail.com>
2022-02-11 10:24:43 +0100

    create Validator class

---
+++ src/components/Validator.ts
@@ -0,0 +1,32 @@
+type TValidator = {
+  type: string
+  errorMessage: string
+}
+
+type GetValidator = (errorMessage: string) => TValidator
+
+export default class Validator {
+  static isPhone: GetValidator = (errorMessage: string) => ({ type: 'IS_PHONE', errorMessage })
+  static isAccessCode: GetValidator = (errorMessage: string) => ({
+    type: 'IS_ACCESS_CODE',
+    errorMessage,
+  })
+
+  static validate(validator: TValidator, value: string) {
+    const { type } = validator
+
+    let isValid = true
+    let errorMessage = ''
+
+    if (type === 'IS_PHONE')
+      isValid =
+        isValid && value.replace(' ', '').length === 9 && /\d{9}/.test(value.replace(' ', ''))
+    if (type === 'IS_ACCESS_CODE')
+      isValid =
+        isValid && value.replace(' ', '').length === 4 && /\d{4}/.test(value.replace(' ', ''))
+
+    if (!isValid) errorMessage = validator.errorMessage
+
+    return errorMessage
+  }
+}
